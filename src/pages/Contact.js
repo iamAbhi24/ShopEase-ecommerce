9bb1f5a8fa2333775fde8f2e7e86ed7327965452
Abhishek Srivastava <abhisheksrivastava199924@gmail.com>
2024-07-21 20:30:06 +0530

    feat:Added loader and alert component in Contact.js -- When Email gets sent a success alert is displaying -- success alert getting disappear after 2 seconds of success -- loader is getting displayed when submiting the form -- after submission the loader is getting disappear

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -3,6 +3,11 @@ import emailjs from "@emailjs/browser";
 import NavBar from "../components/NavBar/NavBar";
 import Footer from "../components/Footer/Footer";
 import TextField from "@mui/material/TextField";
+import CircularProgress from "@mui/material/CircularProgress";
+import Fade from "@mui/material/Fade";
+import Alert from "@mui/material/Alert";
+import AlertTitle from "@mui/material/AlertTitle";
+import Box from "@mui/material/Box";
 
 function Contact() {
   return (
@@ -26,29 +31,36 @@ export function Detail() {
   };
 
   const [submit, setSubmit] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [checked, setChecked] = useState(false);
   // Before submitting the form we are checking all the inputs are valid or not.
   const handleSubmit = (e) => {
-     e.preventDefault();
+    e.preventDefault();
     if (formValidation()) {
-    const emailParams = {
-      subject: "concern",
-      name: state.name,
-      message: state.message,
-      email: state.email,
-    };
-    emailjs
-      .send("service_uy5112s", "contact_form", emailParams, {
-        publicKey: "ZTlmKDsuZ4Q7d4b3s",
-      })
-      .then((response) => {
-        alert(response.text);
-      })
-      .catch((error) => {
-        alert(error);
-      })
-      .finally(()=>{
-        dispatch({type:"reset"})
-      })
+      const emailParams = {
+        subject: "concern",
+        name: state.name,
+        message: state.message,
+        email: state.email,
+      };
+      emailjs
+        .send("service_uy5112s", "contact_form", emailParams, {
+          publicKey: "ZTlmKDsuZ4Q7d4b3s",
+        })
+        .then((response) => {
+          setChecked(true);
+        })
+        .catch((error) => {
+          alert(error);
+        })
+        .finally(() => {
+          dispatch({ type: "reset" });
+          setSubmit(false);
+          setIsLoading(false);
+          setTimeout(() => {
+            setChecked(false);
+          }, 2000);
+        });
     }
   };
   const formValidation = () => {
@@ -59,6 +71,7 @@ export function Detail() {
       !/^[a-z].*@gmail\.com$/.test(state.email.toLowerCase())
     ) {
       setSubmit(true);
+      setIsLoading(false);
       return false;
     } else return true;
   };
@@ -73,38 +86,26 @@ export function Detail() {
       case "message":
         return { ...state, message: action.payload };
       case "reset":
-        return initialvalue ;
+        return initialvalue;
       default:
         return state;
     }
   };
   // Below Reducer function is getting use for seeting state for error input filed in Validation
-  // const errorStateReducer = (errorState, action) => {
-  //   switch (action.type) {
-  //     case "nameMissing":
-  //       return { ...errorState, nameMissing: action.payload };
-  //     case "emailMissing":
-  //       return { ...errorState, emailMissing: action.payload };
-  //     case "messageMissing":
-  //       return { ...errorState, messageMissing: action.payload };
-  //     case "allMissing":
-  //       return { ...errorState, allMissing: action.payload };
-  //     case "emailInvalid":
-  //       return { ...errorState, emailInvalid: action.payload };
-  //     default:
-  //       return errorState;
-  //   }
-  // };
+
   const [state, dispatch] = useReducer(stateReducer, initialvalue);
-  // following uereducer hook is gettting used for storing error state for different input field to implement validation
-  // const [errorState, dispatchValidation] = useReducer(
-  //   errorStateReducer,
-  //   initialErrorValue
-  // );
 
   return (
     <section className="text-gray-600 body-font relative">
-      <div className="container px-5 py-24 mx-auto">
+      <div className="container px-5 py-24 mx-auto ">
+        <Box sx={{ width: "20%", mx: "auto"}}>
+          <Fade in={checked}>
+            <Alert severity="success">
+              <AlertTitle>Success</AlertTitle>
+              Email successfully sent!
+            </Alert>
+          </Fade>
+        </Box>
         <div className="flex flex-col text-center w-full mb-12">
           <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">
             Contact Us
@@ -153,14 +154,18 @@ export function Detail() {
                     error={
                       !state.email && submit
                         ? true
-                        : state.email && submit
+                        : !/^[a-z].*@gmail\.com$/.test(
+                            state.email.toLowerCase()
+                          ) && submit
                         ? true
-                        : ""
+                        : false
                     }
                     helperText={
                       !state.email && submit
                         ? "Enter Email"
-                        : state.email && submit
+                        : !/^[a-z].*@gmail\.com$/.test(
+                            state.email.toLowerCase()
+                          ) && submit
                         ? "Invalid Email"
                         : ""
                     }
@@ -189,40 +194,14 @@ export function Detail() {
               <div className="p-2 w-full">
                 <button
                   className="flex mx-auto text-white bg-slate-900	 border-0 py-2 px-8 focus:outline-none hover:bg-slate-600 rounded text-lg"
-                  // onClick={(e)=>formValidation(e)}
+                  onClick={() => {
+                    setIsLoading(true);
+                  }}
                 >
                   Send Question
                 </button>
+                {isLoading && <CircularProgress color="inherit" />}
               </div>
-              {/* <div class="p-2 w-full pt-8 mt-8 border-t border-gray-200 text-center">
-          <a class="text-indigo-500">example@email.com</a>
-          <p class="leading-normal my-5">49 Smith St.
-            <br/>Saint Cloud, MN 56301
-          </p>
-          <span class="inline-flex">
-            <a class="text-gray-500">
-              <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
-                <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
-              </svg>
-            </a>
-            <a class="ml-4 text-gray-500">
-              <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
-                <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
-              </svg>
-            </a>
-            <a class="ml-4 text-gray-500">
-              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
-                <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
-                <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
-              </svg>
-            </a>
-            <a class="ml-4 text-gray-500">
-              <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
-                <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
-              </svg>
-            </a>
-          </span>
-        </div> */}
             </div>
           </div>
         </form>
